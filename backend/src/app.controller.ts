29e1bc5244325914b78b4b1164aaa1ece97a6714
MarcOreliOoo <albeaurelien@gmail.com>
2023-11-13 12:26:26 +0100

    conf: backend health

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    @Get("health")
+    getHealth(): string {
+        try {
+            return this.appService.getHealth();
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            }
+            throw new HttpException(
+                {
+                    status: HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: error.toString(),
+                },
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                { cause: error },
+            );
+        }
     }
 }
