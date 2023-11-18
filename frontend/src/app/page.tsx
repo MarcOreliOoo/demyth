fd6f064806683b03f8b05c9e79754be5454e104d
MarcOreliOoo <albeaurelien@gmail.com>
2023-11-18 13:23:29 +0100

    add WIP span + rework of card slider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,14 +5,18 @@ import logo from "../../public/demyth_crop.gif";
 // TODO position gradient div at the same level that anim stars and twinkling
 const Home = () => {
     return (
-        <main className="flex flex-col relative items-center justify-center min-h-screen font-spaceGrotesk">
+        <main className="relative flex min-h-screen flex-col items-center justify-center font-spaceGrotesk">
             <div className="background-container">
-                <div className="min-h-screen min-w-full flex items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-black from-25% to-shark">
+                <div className="flex min-h-screen min-w-full items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-black from-25% to-shark">
                     <div className="relative z-10">
                         <Link href="/about">
-                            <Image src={logo} alt="DEMYTH" className="block mx-auto scale-90" />
+                            <Image src={logo} alt="DEMYTH" className="mx-auto block scale-90" />
                         </Link>
+                        <span className="w-full -rotate-[24deg] rounded border-2 border-yellow-500 px-1 py-0.5 text-center text-xl dark:text-yellow-300">
+                            Work In Progress
+                        </span>
                     </div>
+
                     <div className="stars"></div>
                     <div className="twinkling"></div>
                 </div>
