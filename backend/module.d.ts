151e2b793061b1a05152816f0c5840e2ed38b260
MarcOreliOoo <albeaurelien@gmail.com>
2023-11-29 15:13:19 +0100

    add: module env

--- backend/module.d.ts
+++ backend/module.d.ts
@@ -3,6 +3,8 @@ declare namespace NodeJS {
         MONGODB_URL: string;
         JSON_TOKEN_KEY: string;
         JSON_TOKEN_EXPIRE: string;
+        JSON_REFRESH_TOKEN_KEY: string;
+        JSON_REFRESH_TOKEN_EXPIRE: string;
         PORT: string;
     }
 }
