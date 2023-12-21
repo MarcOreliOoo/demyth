b5ad8972f063b27c85f87e1c5934fd34dc08afcd
MarcOreliOoo <albeaurelien@gmail.com>
2023-12-21 14:55:12 +0100

    fix svg clipRule

--- frontend/src/app/(main)/home/hero/page.tsx
+++ frontend/src/app/(main)/home/hero/page.tsx
@@ -1,32 +1,38 @@
+"use client";
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { glassmorphism } from "@/lib/utils/cssProperties";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 const Hero = () => {
     return (
         <section className="widescreen:section-min-height tallscreen:section-min-height relative flex scroll-mt-24 flex-row items-center justify-start gap-6 p-6">
-            <Card>
+            <Card className={`${glassmorphism} `}>
+                <CardHeader>
+                    <CardTitle>MythologyName</CardTitle>
+                </CardHeader>
                 <CardContent>
-                    <p>
-                        Forge Unlikely Alliances In DEMYTH, the bonds you forge are as powerful as the weapons you
-                        wield. Assemble a fellowship with creatures from across all mythologies. Play as a pharaoh
-                        warrior of Ra and recruit the legendary Pegasus of Zeus, or enlist the fearsome Cerberus of
-                        Hades to aid you on your battles.
-                    </p>
-                    <p>
-                        PvP and Resource Battles Engage in intense player-versus-player combat and strategic resource
-                        battles. Every victory earns you resources, paving the way for the evolution of your fellowship,
-                        the expansion of your domain, and the advancement of your hero.
-                    </p>
-                    <p>
-                        NFTs and Marketplace Experience true ownership with our unique NFT system. Your hero and
-                        creatures are yours to collect, trade, and showcase. Explore our integrated marketplace for
-                        exclusive items, creatures, and more.
-                    </p>
-                    <p>
-                        Join Us on this Epic Journey Embark on a legendary adventure like no other. [Your Games Name]
-                        invites you to become a part of a vibrant community of myth-makers, strategists, and conquerors.
-                    </p>
+                    <ScrollArea className="h-[300px]">
+                        <CardDescription>PowerMythologyName</CardDescription>
+                        <p>
+                            Forge Unlikely Alliances In DEMYTH, the bonds you forge are as powerful as the weapons you
+                            wield. Assemble a fellowship with creatures from across all mythologies. Play as a pharaoh
+                            warrior of Ra and recruit the legendary Pegasus of Zeus, or enlist the fearsome Cerberus of
+                            Hades to aid you on your battles.
+                        </p>
+                        <br />
+                        <CardDescription>PowerMythologyName</CardDescription>
+                        <p>
+                            Forge Unlikely Alliances In DEMYTH, the bonds you forge are as powerful as the weapons you
+                            wield. Assemble a fellowship with creatures from across all mythologies. Play as a pharaoh
+                            warrior of Ra and recruit the legendary Pegasus of Zeus, or enlist the fearsome Cerberus of
+                            Hades to aid you on your battles.
+                        </p>
+                    </ScrollArea>
                 </CardContent>
+                <CardFooter>
+                    <CardDescription className="flex w-full items-center justify-end">En savoir plus</CardDescription>
+                </CardFooter>
             </Card>
             <Card className="">
                 <CardContent>
--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -14,17 +14,17 @@ export function Sidebar({ isSidebarOpen, setSidebarOpen }: SidebarProps) {
         <aside
             className={`absolute left-0 top-0 z-[998] h-full bg-opacity-100 backdrop-blur-2xl backdrop-filter transition-all duration-300 ${
                 isSidebarOpen ? "w-64" : "w-20"
-            } border border-red-500`}
+            } border-r`}
         >
             <div className={cn("pb-12")}>
                 <div className="space-y-4 py-4">
                     <div className="px-3 py-2">
-                        <div className="flex flex-row items-center justify-between border border-violet-200">
-                            <h2 className="border border-violet-400 px-7 text-lg font-semibold tracking-tight">Home</h2>
+                        <div className="flex flex-row items-center justify-between">
+                            <h2 className="px-7 text-lg font-semibold tracking-tight">Home</h2>
                             <Button
                                 onClick={() => setSidebarOpen(!isSidebarOpen)}
                                 variant="ghost"
-                                className="justify-start border border-yellow-300"
+                                className="justify-start"
                             >
                                 {isSidebarOpen ? (
                                     <svg
@@ -37,8 +37,8 @@ export function Sidebar({ isSidebarOpen, setSidebarOpen }: SidebarProps) {
                                         <path
                                             d="M6.85355 3.85355C7.04882 3.65829 7.04882 3.34171 6.85355 3.14645C6.65829 2.95118 6.34171 2.95118 6.14645 3.14645L2.14645 7.14645C1.95118 7.34171 1.95118 7.65829 2.14645 7.85355L6.14645 11.8536C6.34171 12.0488 6.65829 12.0488 6.85355 11.8536C7.04882 11.6583 7.04882 11.3417 6.85355 11.1464L3.20711 7.5L6.85355 3.85355ZM12.8536 3.85355C13.0488 3.65829 13.0488 3.34171 12.8536 3.14645C12.6583 2.95118 12.3417 2.95118 12.1464 3.14645L8.14645 7.14645C7.95118 7.34171 7.95118 7.65829 8.14645 7.85355L12.1464 11.8536C12.3417 12.0488 12.6583 12.0488 12.8536 11.8536C13.0488 11.6583 13.0488 11.3417 12.8536 11.1464L9.20711 7.5L12.8536 3.85355Z"
                                             fill="currentColor"
-                                            fill-rule="evenodd"
-                                            clip-rule="evenodd"
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
                                         ></path>
                                     </svg>
                                 ) : (
@@ -52,8 +52,8 @@ export function Sidebar({ isSidebarOpen, setSidebarOpen }: SidebarProps) {
                                         <path
                                             d="M2.14645 11.1464C1.95118 11.3417 1.95118 11.6583 2.14645 11.8536C2.34171 12.0488 2.65829 12.0488 2.85355 11.8536L6.85355 7.85355C7.04882 7.65829 7.04882 7.34171 6.85355 7.14645L2.85355 3.14645C2.65829 2.95118 2.34171 2.95118 2.14645 3.14645C1.95118 3.34171 1.95118 3.65829 2.14645 3.85355L5.79289 7.5L2.14645 11.1464ZM8.14645 11.1464C7.95118 11.3417 7.95118 11.6583 8.14645 11.8536C8.34171 12.0488 8.65829 12.0488 8.85355 11.8536L12.8536 7.85355C13.0488 7.65829 13.0488 7.34171 12.8536 7.14645L8.85355 3.14645C8.65829 2.95118 8.34171 2.95118 8.14645 3.14645C7.95118 3.34171 7.95118 3.65829 8.14645 3.85355L11.7929 7.5L8.14645 11.1464Z"
                                             fill="currentColor"
-                                            fill-rule="evenodd"
-                                            clip-rule="evenodd"
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
                                         ></path>
                                     </svg>
                                 )}
@@ -72,8 +72,8 @@ export function Sidebar({ isSidebarOpen, setSidebarOpen }: SidebarProps) {
                                         <path
                                             d="M7.07926 0.222253C7.31275 -0.007434 7.6873 -0.007434 7.92079 0.222253L14.6708 6.86227C14.907 7.09465 14.9101 7.47453 14.6778 7.71076C14.4454 7.947 14.0655 7.95012 13.8293 7.71773L13 6.90201V12.5C13 12.7761 12.7762 13 12.5 13H2.50002C2.22388 13 2.00002 12.7761 2.00002 12.5V6.90201L1.17079 7.71773C0.934558 7.95012 0.554672 7.947 0.32229 7.71076C0.0899079 7.47453 0.0930283 7.09465 0.32926 6.86227L7.07926 0.222253ZM7.50002 1.49163L12 5.91831V12H10V8.49999C10 8.22385 9.77617 7.99999 9.50002 7.99999H6.50002C6.22388 7.99999 6.00002 8.22385 6.00002 8.49999V12H3.00002V5.91831L7.50002 1.49163ZM7.00002 12H9.00002V8.99999H7.00002V12Z"
                                             fill="currentColor"
-                                            fill-rule="evenodd"
-                                            clip-rule="evenodd"
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
                                         ></path>
                                     </svg>
                                     Albums
